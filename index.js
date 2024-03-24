e5fda4c78bd00507114921b76bdf033ec04f5ef3
muskan-fatim <160476952+muskan-fatim@users.noreply.github.com>
2024-03-24 01:05:30 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+import inquirer from 'inquirer';
+const random = Math.floor(Math.random() * 6 + 1);
+const answer = await inquirer.prompt([{
+        name: "game",
+        type: "number",
+        message: "Guess the number between 1-6:"
+    }
+]);
+if (answer.game === random) {
+    console.log("congratulation you guess the right number");
+}
+else {
+    console.log("you guess is wrong try again");
+}
